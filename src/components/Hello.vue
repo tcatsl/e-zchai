1d234b90fd894657c1ddeb06bbcf89da1bb3e856
tcatsl <thomas.castleman@gmail.com>
2017-05-31 21:49:09 -0600

    test generation working

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -6,19 +6,21 @@ Code:
 var x = 9
 </code></pre>
 <div id="mocha"></div>
-<button id = "runTest" v-on:click="rUn()">Run tests</button>
+<button id="add" v-on:click="addTest()">Add Test</button>
+<button id = "runTest" v-on:click="rUn()">Run Tests</button>
+
 Tests:
-</button>
-<pre v-show="true == false"><code class=" hidden lang-eval-js" data-external-libs="https://cdn.rawgit.com/mochajs/mocha/2.2.5/mocha.js, http://chaijs.com/chai.js">
+<pre v-show="true == false"><code class=" hidden lang-eval-js" data-external-libs="https://cdn.rawgit.com/mochajs/mocha/2.2.5/mocha.js, https://cdnjs.cloudflare.com/ajax/libs/chai/4.0.0/chai.min.js" data-loop-msec="1000">
+</code>
+</pre>
+<pre id="tests"><code id="testcode" class="lang-eval-js" data-external-libs="https://cdn.rawgit.com/mochajs/mocha/2.2.5/mocha.js, https://cdnjs.cloudflare.com/ajax/libs/chai/4.0.0/chai.min.js">
 assert = chai.assert
 mocha.suite.suites = []
 mocha.setup("bdd")
-</code>
-</pre>
-<pre id="tests"><code class="lang-eval-js" data-external-libs="https://cdn.rawgit.com/mochajs/mocha/2.2.5/mocha.js, http://chaijs.com/chai.js">
-  describe('x', function() {
-    it('should not equal 9', function() {
-      assert.notEqual(9, x);
+
+describe('x', function() {
+  it('should not equal 9', function() {
+    assert.notEqual(9, x);
   });
 });
 </code></pre>
@@ -27,24 +29,21 @@ mocha.setup("bdd")
 </template>
 
 <script>
+var vm;
 var assertions = ["fail","isOk","isNotOk","equal","notEqual","strictEqual","notStrictEqual","deepStrictEqual","deepEqual","notDeepEqual","isAbove","isAtLeast","isBelow","isAtMost","isTrue","isNotTrue","isFalse","isNotFalse","isNull","isNotNull","isNaN","isNotNaN","exists","notExists","isUndefined","isDefined","isFunction","isNotFunction","isObject","isNotObject","isArray","isNotArray","isString","isNotString","isNumber","isNotNumber","isFinite","isBoolean","isNotBoolean","typeOf","notTypeOf","instanceOf","notInstanceOf","include","notInclude","deepInclude","notDeepInclude","nestedInclude","notNestedInclude","deepNestedInclude","notDeepNestedInclude","ownInclude","notOwnInclude","deepOwnInclude","notDeepOwnInclude","match","notMatch","property","notProperty","propertyVal","notPropertyVal","deepPropertyVal","notDeepPropertyVal","ownProperty","notOwnProperty","ownPropertyVal","notOwnPropertyVal","deepOwnPropertyVal","notDeepOwnPropertyVal","nestedProperty","notNestedProperty","nestedPropertyVal","notNestedPropertyVal","deepNestedPropertyVal","notDeepNestedPropertyVal","lengthOf","hasAnyKeys","hasAllKeys","containsAllKeys","doesNotHaveAnyKeys","doesNotHaveAllKeys","hasAnyDeepKeys","hasAllDeepKeys","containsAllDeepKeys","doesNotHaveAnyDeepKeys","doesNotHaveAllDeepKeys","throws","doesNotThrow","operator","closeTo","approximately","sameMembers","notSameMembers","sameDeepMembers","notSameDeepMembers","sameOrderedMembers","notSameOrderedMembers","sameDeepOrderedMembers","notSameDeepOrderedMembers","includeMembers","notIncludeMembers","includeDeepMembers","notIncludeDeepMembers","includeOrderedMembers","notIncludeOrderedMembers","includeDeepOrderedMembers","notIncludeDeepOrderedMembers","oneOf","changes","changesBy","doesNotChange","changesButNotBy","increases","increasesBy","doesNotIncrease","increasesButNotBy","decreases","decreasesBy","doesNotDecrease","doesNotDecreaseBy","decreasesButNotBy","ifError","isExtensible","isNotExtensible","isSealed","isNotSealed","isFrozen","isNotFrozen","isEmpty","isNotEmpty","ok","notOk","throw","Throw","extensible","notExtensible","sealed","notSealed","frozen","notFrozen","empty","notEmpty"]
-function runMochaTests() {
-  Object.keys( require.cache ).forEach( function( file ) {
-      delete require.cache[ file ];
-  } );
 
-  var mocha = new Mocha();
-
-  mocha.addFile( 'run_test.js' );
-
-  mocha.run();
-}
  function reLoad(){
-
+  if (!!script){
+    script = null
+  }
   var script = document.createElement("script");
   script.id = "repl"
   script.src = "https://storage.googleapis.com/app.klipse.tech/plugin_prod/js/klipse_plugin.min.js";
+
    document.body.append(script)
+   setTimeout(function(){
+   var final = document.getElementsByClassName("cm-s-default")
+   final[final.length -1 ].innerHTML = ""}, 1000)
 }
 function resetTests(suite) {
 suite.tests.forEach(function(t) {
@@ -55,13 +54,40 @@ suite.suites.forEach(resetTests);
 }
 export default {
   name: 'hello',
+  data: function(){
+  return {
+  tests: []
+  }
+  },
   methods: {
   rUn: function(){
   document.getElementById("mocha").innerHTML = ""
 resetTests(mocha.suite);
 mocha.run();
+  },
+  addTest: function(){
+  this.tests.push({describe: {name:'x', its:
+  [{itsDescr: 'should not equal 9', assertions: [{assert:'notEqual', p1: 'x', p2: '9', p3: null, descr:'should not equal 9'}]}]}})
+  var code = ''
+  for (var i = 0; i < this.tests.length; i++){
+  var itsCode = '';
+  for (var its = 0; its < this.tests[i].describe.its.length; its++){
+  console.log(this.tests[i].describe.its[its])
+  var assertCode = '';
+  for (var ass = 0; ass < this.tests[i].describe.its[its].assertions.length; ass++){
+  assertCode += 'assert.'+this.tests[i].describe.its[its].assertions[ass].assert+'('+this.tests[i].describe.its[its].assertions[ass].p1+', '+this.tests[i].describe.its[its].assertions[ass].p2+', )\n'
+  }
+   itsCode += 'it("'+this.tests[i].describe.its[its].itsDescr+'", function(){\n'+assertCode+'})\n'
+  }
+    code += 'describe("'+this.tests[i].describe.name+'", function(){\n'+ itsCode +'})'
+  }
+  tests.innerHTML = '<code class="lang-eval-js">'+ 'assert = chai.assert\n mocha.suite.suites = []\n mocha.setup("bdd")\n'+ code+'</code>'
+  reLoad()
   }
   },
-  mounted: reLoad()
+  mounted: function(){reLoad();
+  vm = this;
+  vm.assertions = assertions
+  }
 }
 </script>

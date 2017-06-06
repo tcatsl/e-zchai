537485e43a606ba5ab50b6e1119019874ae49a9a
tcatsl <thomas.castleman@gmail.com>
2017-06-06 00:06:41 -0600

    ui stuff

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -20,7 +20,7 @@ var x = 9
 
 <v-expansion-panel id="foo" v-xdo>
     <v-expansion-panel-content v-xdo v-bind:value="index === tests.length-1"v-for="(test, index) in tests" :key="index" >
-      <div slot="header"><v-btn floating small v-on:click.native.capture.stop.prevent="removeTest(index)" class="red lighten-1"><v-icon>clear</v-icon></v-btn><span class="text-xs-center"><v-btn floating small v-show="test.describe.editingDescr == false" v-on:click.native.capture.stop.prevent="editDescr(test.describe)"><v-icon>edit</v-icon></v-btn><v-btn floating small v-show="test.describe.editingDescr == true" v-on:click.native.capture.stop.prevent="finishEditDescr(test.describe)" class="green accent-1"><v-icon>done</v-icon></v-btn></span>{{test.describe.name}}<div v-show="test.describe.editingDescr == true">
+      <div class="descrhead" slot="header"><v-btn floating small v-on:click.native.capture.stop.prevent="removeTest(index)" class="red lighten-1"><v-icon>clear</v-icon></v-btn><span class="text-xs-center"><v-btn floating small v-show="test.describe.editingDescr == false" v-on:click.native.capture.stop.prevent="editDescr(test.describe)"><v-icon>edit</v-icon></v-btn><v-btn floating small v-show="test.describe.editingDescr == true" v-on:click.native.capture.stop.prevent="finishEditDescr(test.describe)" class="green accent-1"><v-icon>done</v-icon></v-btn></span> &nbsp;{{test.describe.name}}<div v-show="test.describe.editingDescr == true">
       <v-text-field autofocus v-on:click.native.capture.prevent.stop="func" label="The name of the thing you are testing:" v-model="test.describe.name"></v-text-field>
       </div></div>
       <div>
@@ -32,8 +32,8 @@ var x = 9
       </p>
       <v-expansion-panel v-do>
       <v-expansion-panel-content v-xdo v-bind:value="index4 === test.describe.its.length-1" v-for="(it, index4) in test.describe.its" :key="index4" >
-      <div slot="header">
-      <v-btn floating small v-on:click.native.capture.stop.prevent="removeIts(index, index4)" class="red lighten-1" ><v-icon>clear</v-icon></v-btn><v-btn floating small v-show="it.editingIt == false && addingIt == false" v-on:click.native.stop.prevent="editIt(it)"><v-icon>edit</v-icon></v-btn><v-btn floating small class="green accent-1" v-show="it.editingIt == true" v-on:click.native.capture.stop.prevent="finishEditIt(it)"><v-icon>done</v-icon></v-btn> {{it.itsDescr}} <v-text-field v-show="it.editingIt == true" v-on:click.native.capture.prevent.stop="func" label="(should...):" v-model="it.itsDescr"></v-text-field>
+      <div class="itshead" slot="header">
+      <v-btn floating small v-on:click.native.capture.stop.prevent="removeIts(index, index4)" class="red lighten-1" ><v-icon>clear</v-icon></v-btn><v-btn floating small v-show="it.editingIt == false && addingIt == false" v-on:click.native.stop.prevent="editIt(it)"><v-icon>edit</v-icon></v-btn><v-btn floating small class="green accent-1" v-show="it.editingIt == true" v-on:click.native.capture.stop.prevent="finishEditIt(it)"><v-icon>done</v-icon></v-btn> &nbsp;{{it.itsDescr}} <v-text-field v-show="it.editingIt == true" v-on:click.native.capture.prevent.stop="func" label="(should...):" v-model="it.itsDescr"></v-text-field>
       </div>
       <v-card>
       <v-card-text>
@@ -42,12 +42,12 @@ var x = 9
       </p>
       <v-expansion-panel  v-do>
       <v-expansion-panel-content @click.native="editAss(index, index4, ass, $event)" v-xdo v-bind:value="index5 === it.assertions.length-1" v-bind:key="'ass' + index5"  v-for="(ass, index5) in it.assertions"  >
-      <div  slot="header">
-      <v-btn floating small v-on:click.native.capture.stop.prevent="removeAss(index, index4, index5)" class="red lighten-1"><v-icon>clear</v-icon></v-btn><v-btn class="green accent-1" v-show="(ass.editingAss== true)" floating small><v-icon>done</v-icon></v-btn><v-btn floating small v-show="(ass.editingAss== false)"><v-icon>edit</v-icon></v-btn> {{ass['p'+(ass.params.length)]}}
+      <div @click.native="editAss(index, index4, ass, $event)" slot="header">
+      <v-btn floating small v-on:click.native.capture.stop.prevent="removeAss(index, index4, index5)" class="red lighten-1"><v-icon>clear</v-icon></v-btn><v-btn class="green accent-1" v-show="(ass.editingAss== true)" floating small><v-icon>done</v-icon></v-btn><v-btn floating small v-show="(ass.editingAss== false)"><v-icon>edit</v-icon></v-btn>&nbsp;&nbsp;{{ass['p'+(ass.params.length)]}}
     </div>
-      <v-card @click.native.capture="editAss(index, index4, ass, $event)">
+      <v-card>
       <v-card-text>
-      <div class="eeee">
+      <div class="eeee" >
       <v-select id="editAssSelect" ref="editAssSelect" autofocus :autocomplete="true" auto v-bind:items="assertions" :on-change="func(ass)" @keydown.tab.capture.native="tab2($event)"  v-model="ass.assert" label="Assertion:"></v-select>
       <v-text-field @click.native.capture.stop.prevent="" ref="stuffo" v-for="(param, index9) in ass.params" v-bind:label="param" v-model="ass['p'+ (index9+1)]"></v-text-field>
       </div>
@@ -108,6 +108,7 @@ export default {
   name: 'hello',
   data: function(){
   return {
+    code: '',
   re1: /expansion-panel__header--active/g,
   assertions: ["assert","fail","isOk","isNotOk","equal","notEqual","strictEqual","notStrictEqual","deepEqual","notDeepEqual","isAbove","isAtLeast","isBelow","isAtMost","isTrue","isNotTrue","isFalse","isNotFalse","isNull","isNotNull","isNaN","isNotNaN","exists","notExists","isUndefined","isDefined","isFunction","isNotFunction","isObject","isNotObject","isArray","isNotArray","isString","isNotString","isNumber","isNotNumber","isFinite","isBoolean","isNotBoolean","typeOf","notTypeOf","instanceOf","notInstanceOf","include","notInclude","deepInclude","notDeepInclude","nestedInclude","notNestedInclude","deepNestedInclude","notDeepNestedInclude","ownInclude","notOwnInclude","deepOwnInclude","notDeepOwnInclude","match","notMatch","property","notProperty","propertyVal","notPropertyVal","deepPropertyVal","notDeepPropertyVal","nestedProperty","notNestedProperty","nestedPropertyVal","notNestedPropertyVal","deepNestedPropertyVal","notDeepNestedPropertyVal","lengthOf","hasAnyKeys","hasAllKeys","containsAllKeys","doesNotHaveAnyKeys","doesNotHaveAllKeys","hasAllDeepKeys","containsAllDeepKeys","doesNotHaveAnyDeepKeys","doesNotHaveAllDeepKeys","throws","doesNotThrow","operator","closeTo","approximately","sameMembers","notSameMembers","sameDeepMembers","notSameDeepMembers","sameOrderedMembers","notSameOrderedMembers","sameDeepOrderedMembers","notSameDeepOrderedMembers","includeMembers","notIncludeMembers","includeDeepMembers","notIncludeDeepMembers","includeOrderedMembers","notIncludeOrderedMembers","includeDeepOrderedMembers","notIncludeDeepOrderedMembers","oneOf","changes","changesBy","doesNotChange","changesButNotBy","increases","increasesBy","doesNotIncrease","increasesButNotBy","decreases","decreasesBy","doesNotDecrease","decreasesButNotBy","ifError","isExtensible","isNotExtensible","isSealed","isNotSealed","isFrozen","isNotFrozen","isEmpty","isNotEmpty"].map(function(el, ind, arr){
   return {text: el, value: el}
@@ -192,8 +193,8 @@ setTimeout(() => {
   this.buildTests();
   },
   editAss: function(index, index2, ass, e){
-    // alert(e.currentTarget.parentNode.children[0].children[1].classList.toString())
- if (e.currentTarget.parentNode.children[0].children[1].classList.contains('v-enter') || e.currentTarget.parentNode.children[0].children[1].classList.contains('v-leave')){
+ if
+ (e.srcElement.classList.toString() != 'card__text'){
   if (ass.editingAss == false){
   for (var h = 0; h < this.tests[index].describe.its[index2].assertions.length; h++){
   this.tests[index].describe.its[index2].assertions[h].editingAss = false;
@@ -365,6 +366,7 @@ mocha.run();
     code += 'describe("'+this.tests[i].describe.name+'", function(){\n'+ itsCode +'})\n'
   }
   tests.innerHTML = '<code class="lang-eval-js">'+ 'assert = chai.assert\n mocha.suite.suites = []\n'+ code+'</code>'
+  this.code=code
   reLoad()
   }
   },
@@ -478,6 +480,9 @@ width: 100%
 .expansion-panel__header--active:after{
 float: right
 }
+#mocha-stats{
+  position: static !important;
+}
 .expansion-panel__header{
 padding-top: 0px;
 display: inline-flex;
@@ -537,10 +542,13 @@ margin-left: 0;
 box-shadow: none
 }
 .card__text {
-  padding-left: 100px;
+  margin-left: 50px;
 padding-top: 2px;
 padding-bottom: 0px;
 padding-right: 0
+}
+.expansion-panel .expansion-panel .expansion-panel .card__text {
+  margin-left: 0;
 }
 .card__text p {
 margin-bottom: 3px
@@ -572,5 +580,7 @@ height: auto !important
 }
 .input-group{
   padding-right: 13px
+}
+.itshead .input-group, .descrhead .input-group{
 }
 </style>
--- src/components/auth.vue
+++ src/components/auth.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-        <h5>e-zchai<v-btn id="logOut" v-if="isLoggedIn()" class="btn btn-danger log"  v-on:click.native>Log out</v-btn>
+        <h5><b>e-zchai</b><v-btn id="logOut" v-if="isLoggedIn()" class="btn btn-danger log"  v-on:click.native>Log out</v-btn>
         <v-btn id="logIn" v-if="!isLoggedIn()"class="btn btn-info log"  v-on:click.native="handleLogin()">Log In</v-btn></h5>
   </div>
 </template>
